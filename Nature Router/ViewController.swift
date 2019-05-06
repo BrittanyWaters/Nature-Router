a535b15e4e55b815dc760786dba39e46fc049af6
Brittany Waters <brittany.waters@smail.astate.edu>
2019-05-06 02:44:02 -0500

    Initial Commit

---
+++ Nature Router/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Nature Router
+//
+//  Created by Brittany.Waters on 5/6/19.
+//  Copyright © 2019 Brittany.Waters. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
